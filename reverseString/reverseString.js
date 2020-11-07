5de680596965fdfc8887271e49508c1d5d8db006
erencataltepe <erencataltepe@gmail.com>
2020-11-07 10:31:41 +0300

    Solve reverseString problem.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const arrayOfString = string.split("");
+  const reverseArray = arrayOfString.reverse();
+  const reverseString = reverseArray.join("");
+  
+  return reverseString;
 }
 
 module.exports = reverseString
