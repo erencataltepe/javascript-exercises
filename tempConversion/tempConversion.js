ae69f193ca6f81930d214d3f1924cdc4649313eb
erencataltepe <erencataltepe@gmail.com>
2020-11-07 11:16:15 +0300

    Solve tempConversion problem.

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  const celc = (fahr - 32) * (5 / 9);
+  return Math.round(celc * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(celc) {
+  const fahr = celc * (9 / 5) + 32;
+  return Math.round(fahr * 10) / 10;
 }
 
 module.exports = {
