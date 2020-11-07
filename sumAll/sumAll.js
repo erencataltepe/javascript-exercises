f7f478e5780b9efe5d1eb197dd246deff6acda56
erencataltepe <erencataltepe@gmail.com>
2020-11-07 11:00:33 +0300

    Solve sumAll problem.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let total = 0;
+  let startNum, endNum;
 
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (num1 < num2) {
+    startNum = num1;
+    endNum = num2;
+  } else {
+    startNum = num2;
+    endNum = num1;
+  }
+
+  for (let i = startNum; i <= endNum; i++) {
+    total += i;
+  }
+
+  return total;
 }
 
 module.exports = sumAll
