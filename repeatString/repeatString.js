58d283b4ee1680b591d519a2c87487c196d44817
erencataltepe <erencataltepe@gmail.com>
2020-11-07 10:28:52 +0300

    Solve repeatString problem.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, interval) {
+  let result = "";
+  
+  if (interval < 0) {
+    return "ERROR";
+  }
+  
+  for (let i = 0; i < interval; i++) {
+    result += string;
+  }
 
+  return result;
 }
 
 module.exports = repeatString
