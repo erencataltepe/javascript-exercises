ea65a9089eca2de6a09eebd1750a961478c74d47
erencataltepe <erencataltepe@gmail.com>
2020-11-07 10:44:52 +0300

    Solve removeFromArray problem.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemovedValues) {
+  
+  for (let i = 0; i < toBeRemovedValues.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === toBeRemovedValues[i]) {
+        array.splice(j, 1);
+      }
+    }
+  }
 
+  return array;
 }
 
 module.exports = removeFromArray
