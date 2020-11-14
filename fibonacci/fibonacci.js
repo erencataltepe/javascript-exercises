26fe97219e01276b5f0a0c914a3c0311ff72827c
erencataltepe <erencataltepe@gmail.com>
2020-11-14 20:18:34 +0300

    Solve fibonacci exercise.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
 
+  if (typeof n === "string") {
+    n = Number(n);
+  }
+
+  let pre = 1;
+  let next = 1;
+  let tempNext;
+
+  if (n == 1 || n == 2) {
+    return 1;
+  } 
+
+  for (let i = 2; i < n; i++) {
+    tempNext = pre + next;
+    pre = next;
+    next = tempNext;
+  }
+
+  return tempNext;
 }
 
 module.exports = fibonacci
