c8c0382c97ba4279c6a1d1f7e5680023ee100519
erencataltepe <erencataltepe@gmail.com>
2020-11-14 09:48:20 +0300

    Solve palindromes exercise.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+  const arr = word.toLowerCase().split("").filter((char) => char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 122);
+  const reverseArr = [...arr].reverse();
+  for (let i = 0; i < reverseArr.length; i++) {
+    if (reverseArr[i] != arr[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = palindromes
