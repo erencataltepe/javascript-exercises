284c5bbe58207184e4b7cb1763fd2425994148dd
erencataltepe <erencataltepe@gmail.com>
2020-11-14 07:24:29 +0300

    Finish calculator exercise.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,35 @@
-function add () {
-	
+function add (a,b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.reduce((total, item) => total + item, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((total, item) => total * item, 1);
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num == 0 || num == 1)  {
+		return 1;
+	} else {
+		let count = 1;
+		let result = 1;
+		while (count <= num) {
+			result *= count;
+			count += 1;
+		}
+		return result;		
+	}
 }
 
 module.exports = {
