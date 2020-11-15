ecdea6e2889154d5bba4d73f9b4ba8ce4785aec9
erencataltepe <erencataltepe@gmail.com>
2020-11-15 08:46:20 +0300

    Solve findTheOldest exercise.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+  let oldest = people[0];
 
+  people.forEach((person) => {
+    const currentYear = new Date().getFullYear();
+    let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    let currentPersonAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    if (currentPersonAge > oldestAge) {
+      oldest = person;
+    }
+  })
+
+  return oldest;
 }
 
 module.exports = findTheOldest
