0300bed7759947fd9643894da56c1fe026a72414
Eren Çataltepe <erencataltepe@gmail.com>
2026-02-18 13:41:09 +0100

    Solve sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
 
+  let total = 0;
+  let start = a <= b ? a : b;
+  let end = b >= a ? b : a;
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
