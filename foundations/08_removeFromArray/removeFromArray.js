cfab9199febea5dc44bf8f239ce9697ec6e32fb2
Eren Çataltepe <erencataltepe@gmail.com>
2026-02-18 13:32:46 +0100

    Solve removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...arg) {
+  const newArr = [];
+  for (let el of arr) {
+    if (!arg.includes(el)) {
+      newArr.push(el);
+    }
+  }
+
+  return newArr;
 };
 
 // Do not edit below this line
