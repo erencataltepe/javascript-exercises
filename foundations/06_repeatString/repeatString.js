2a86eb80ad049799ed20dec57e488692e06f75d9
Eren Çataltepe <erencataltepe@gmail.com>
2026-02-18 11:44:09 +0100

    Solve exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, repeatCount) {
+  const lastWord = [];
 
+  if (repeatCount < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < repeatCount; i++) {
+    lastWord.push(word);
+  }
+
+  return lastWord.join("");
 };
 
 // Do not edit below this line
